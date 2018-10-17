3bbc915306ad479a26ad1e59a371c6746bc195fd
Rebecca Leiß <rele18ab@student.cbs.dk>
2018-10-17 15:36:48 +0200

    try to display products from array in html...not working yet

--- js/products.js
+++ js/products.js
@@ -15,5 +15,13 @@ var products = [];
 products.push(new Product("1","Bella Italia","Backpacking","Italy","10","100"));
 products.push(new Product("2", "Party in Paris", "City Trip", "France", "3", "200"));
 
+
+// display products ---- NOT WORKING 
+var listproducts = document.getElementById('listproducts');
+
 console.log(products);
 
+document.getElementById('listproducts').innerHTML = products;
+
+
+// TO DO: figure out how to display array in HTML
