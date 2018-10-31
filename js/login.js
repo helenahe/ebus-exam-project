3e4483d013efac710dfae98dd4d2a2b33b5c5730
Rebecca Leiß <rele18ab@student.cbs.dk>
2018-10-31 14:45:25 +0100

    fixed loading issue

--- js/login.js
+++ js/login.js
@@ -1,73 +1,75 @@
 // get input from login for and set as variables
-    var inputEmail = document.getElementById('emailaddress');
-    var inputPassword = document.getElementById('password')
+var inputEmail = document.getElementById('emailaddress');
+var inputPassword = document.getElementById('password')
 
 // hard code some users (replace later with JSON file)
-    // 1. creating empty user array
-    var users = [];
-    // 2. filling user array
-    users.push(new User("Anna", "Smith", "anna@gmail.com", "1"));
-    users.push(new User("Ben", "Williams", "ben@gmail.com", "2"));
+// 1. creating empty user array
+var users = [];
+// 2. filling user array
+users.push(new User("Anna", "Smith", "anna@gmail.com", "1"));
+users.push(new User("Ben", "Williams", "ben@gmail.com", "2"));
 
-    console.log(users); // check users
+console.log(users); // check users
 
 // setting allowed login attempts to block user after 3 unsuccessful tries
-    var attempts = 3; 
+var attempts = 3;
 
 // connect variable to resultLogin to display login output in html
-    var resultSpan = document.getElementById('loginResult');
+var resultSpan = document.getElementById('loginResult');
 
 // binding submit button to variable
-    var submit = document.getElementById('submit');
+var submit = document.getElementById('submit');
 
-    // executes login function when enter is pressed   (source: https://stackoverflow.com/questions/16011312/execute-function-on-enter-key) 
-    submit.addEventListener("keydown", function(enter) {
-        if (enter.keyCode === 13) { // key === 13 stands for enter/return key
-            login(enter);
-        }
-    })
+// NOT WOKRING creating variable that stores whether the user is logged in or not. Starts as false, changes to true on successful log in 
+var isLoggedIn = false
+console.log(isLoggedIn);
+
+// executes login function when enter is pressed   (source: https://stackoverflow.com/questions/16011312/execute-function-on-enter-key) 
+submit.addEventListener("keydown", function (enter) {
+    if (enter.keyCode === 13) { // key === 13 stands for enter/return key
+        login(enter);
+    }
+})
 // start login function by clicking submit
-    submit.onclick = function login() {
+submit.onclick = function login() {
 
-//telling user to enter credentials, in case they submit empty fields
-    if(inputEmail.value.length == 0 || inputPassword.value.length == 0){
+    //telling user to enter credentials, in case they submit empty fields
+    if (inputEmail.value.length == 0 || inputPassword.value.length == 0) {
         resultSpan.innerText = "You need to enter your email address and password in order to log in";
         return false;
-    } 
-
-// Loop through users and return true if matching
-    for(var i = 0; i < users.length; i++) {
-
-// Bind user to a variable  
-    var user = users[i]; 
-
-    console.log(i) // checking if loop works
-
-//  Successful login in input matches user credentials
-   if (inputEmail.value == user.emailaddress && inputPassword.value == user.password) {
-            resultSpan.innerText = "Congrats "  + user.firstname + " " + user.lastname + ", you actually remembered your password correctly";
-            return true;
-        }
     }
 
-// block user if too many failed login attempts.
-            if (attempts == 0){
-                resultSpan.innerText = "You've entered the wrong credentials 3 times. For security purposes, we've blocked you from our system";
-                inputEmail.disabled = true;
-                inputPassword.disabled = true; 
-                submit.disabled = true;
-                return false;
-                
-             } else {
-                attempts--; //subtract one from allowed attempts 
-                resultSpan.innerText = "Nice try! Go again. You have " + attempts + " more attempts to get it right.";
-                console.log(attempts); // check if counter works
-                return false;
-                }
+    // Loop through users and return true if matching
+    for (var i = 0; i < users.length; i++) {
 
-                
+        // Bind user to a variable  
+        var user = users[i];
 
-          
+        console.log(i) // checking if loop works
+
+        //  Successful login if input matches user credentials
+        if (inputEmail.value == user.emailaddress && inputPassword.value == user.password) {
+            resultSpan.innerText = "Congrats " + user.firstname + " " + user.lastname + ", you actually remembered your password correctly";
+            isLoggedIn = true;// not working
+            return false;
+
+        }
+    }
+    console.log(isLoggedIn); // check if loginStatus changed upon successful login
+    // block user if too many failed login attempts.
+    if (attempts == 0) {
+        resultSpan.innerText = "You've entered the wrong credentials 3 times. For security purposes, we've blocked you from our system";
+        inputEmail.disabled = true;
+        inputPassword.disabled = true;
+        submit.disabled = true;
+        return false;
+
+    } else {
+        attempts--; //subtract one from allowed attempts 
+        resultSpan.innerText = "Nice try! Go again. You have " + attempts + " more attempts to get it right.";
+        console.log(attempts); // check if counter works
+        return false;
+    }
 
 }; // close function
 
@@ -76,6 +78,7 @@
 // To do:
 // - store password as hashpassword
 // - redirect user to new site, when logged in? 
+// - change status to "logged in"
 // - display sign out button, disable login once logged in
 // - what is last Access all about?
 //      - enable login again after periode of time, after being blocked for too many failed attempts??
