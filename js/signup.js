16bb258116460dfbb3b64d6f4974a49eb2610f35
Helena Helgadóttir <helenah90@gmail.com>
2018-11-17 22:33:32 +0100

    Validation, checking user database and more

--- js/signup.js
+++ js/signup.js
@@ -25,20 +25,31 @@ submit.onclick = function signup(e) {
 
     // make sure that user fills out all fields  
     if (inputFirstname.value.length === 0 || inputLastname.value.length === 0 || inputEmail.value.length === 0 || inputPassword.value.length === 0 || inputRepeatPassword.value.length === 0) {
-        resultSpan.innerText = "You need to fill out all fields in order to sign up";
+        resultSpan.innerText = 'You need to fill out all fields in order to sign up';
         return false;
     }
     // check if password and confirm password match  
     if (inputPassword.value !== inputRepeatPassword.value) {
-        resultSpan.innerText = "Passwords don't match.";
+        resultSpan.innerText = 'Passwords don\'t match.';
         return false;
     }
+
+    // check if the email address is already stored in local storage
+    for (i = 0; i < users.length; i++) {
+        if (users[i].emailaddress === inputEmail.value) {
+            resultSpan.innerText = 'This email address is already in use';
+            return false;
+        } 
+
+    }
+
     // store sign up input in new user
     var newUser = new User(
         inputFirstname.value,
         inputLastname.value,
         inputEmail.value,
         inputPassword.value,
+        Date()
     );
 
     // push user into users array
@@ -50,12 +61,15 @@ submit.onclick = function signup(e) {
 
     resultSpan.innerHTML = "Sign up successful <a href = login.html> click here to log in </a>";
 
+    document.getElementById("signup_form").reset();
     // debugger;
 }
 
- // TODO store sign up input in new user - so you can use to login and (done)
- // TODO display successful sign up message and redirect to login, once signed up (done)
- // TODO check password strength. 
- // TODO validate email address (making sure that user puts in a real email address, and not just some text)
- // TODO check against user database, if this email address is already signed up. 
- // TODO register date of registration 
+ // TODO store sign up input in new user - so you can use to login and (DONE)
+ // TODO display successful sign up message and redirect to login, once signed up (DONE)
+ // TODO check password strength. (DONE)
+ // TODO validate email address (making sure that user puts in a real email address, and not just some text). (DONE)
+ // TODO register date of registration. (DONE)
+ // TODO check against user database, if this email address is already signed up. (DONE)
+ // TODO Hash password when storing it.
+ // TODO Make the info disappear from the text fields when signing in. (DONE)
--- js/user.js
+++ js/user.js
@@ -1,9 +1,10 @@
 class User {
 
-    constructor(firstname, lastname, emailaddress, password) {
+    constructor(firstname, lastname, emailaddress, password, createdDate) {
         this.firstname = firstname;
         this.lastname = lastname;
         this.emailaddress = emailaddress;
         this.password = password;
+        this.createdDate = createdDate;
     }
 }
